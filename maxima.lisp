8e2bcd5b3888de84f08fbfc48954bf58c653d1c7
Ben Diedrich <bldiedrich@gmail.com>
2013-03-18 17:54:18 -0700

    Removed all references to external Maxima invocation. Everything is through the embeddable-maxima package.

--- maxima.lisp
+++ maxima.lisp
@@ -1,53 +1,37 @@
-;; Maxima interface
-;; Converts CL math expressions to Maxima equivalent and evaluates them in Maxima's Lisp level to simplify them
-;; Results are returned and extracted from the Maxima output
+;;;; Maxima interface for simplifying Lisp math expressions
+
+;;; Converts Lisp math expressions to Maxima equivalents
+;;; Simplifies in Maxima's Lisp level
+
+;;; TODO
+;;; Fix Jacobi method for finding eigenvalues & eigenvectors of matrices
+;;; Access other Maxima features for enhanced mathematics in Lisp
 
 (defpackage :bld-maxima
   (:use :common-lisp :cl-ppcre)
   (:export :*delay*
-;;	   :*maxima-binary*
-;;	   :*maxima-batch-options*
-;;	   :*maxima-init-expressions*
-;;	   :*maxima-lisp-table*
 	   :simp
 	   :simp-exprs
 	   :jacobi
-;;	   :run-maxima-command
-;;	   :run-maxima-lisp
-;;	   :*maxima-port*
-;;	   :*maxima-socket-options*
-;;	   :*maxima-socket-init-forms*
-;;	   :*maxima-host*
-;;	   :*maxima-socket-passive*
-;;	   :*maxima-socket*
-;;	   :*maxima-pid*
-;;	   :maxima-start
-;;	   :maxima-shutdown
-;;	   :maxima-read
-;;	   :maxima-send
-;;	   :maxima-send-lisp
-;;	   :simp-socket
-;;	   :jacobi-socket
 	   :atan2
-	   :delay))
+	   :delay
+	   :%pi))
 
 ;; Reset from change made by EMBEDDABLE-MAXIMA
 (setq cl:*read-default-float-format* 'single-float)
 
 (in-package :bld-maxima)
 
+;; Define %PI variable to be Maxima %PI for simplifying trig relationships
+(defparameter %pi 'maxima::$%pi)
+
 (defvar *delay* nil "Check whether to defer evaluation")
-;;(defparameter *maxima-binary* "/usr/bin/maxima")
-;;(defparameter *maxima-batch-options* "-q --batch-string \"display2d : false$ ")
-;;(defvar *maxima-init-expressions* nil)
 (defparameter *maxima-lisp-table*
   '((mplus +)
     (mminus -)
     (mtimes *)
     (mexpt expt)
     (mlist list)
-;;    (mequal =) ; comparison operators not really useful with Maxima
-;;    (mgreaterp >)
     (mabs abs)
     (mquotient /)
     (%sin sin)
@@ -63,44 +47,17 @@
     (%asin asin)
     (%acos acos)
     (%signum signum)
-    (%max max))
+    (%max max)
+    (rat /))
   "lookup table of maxima > lisp expressions")
 
 (defmethod atan2 ((n1 number)(n2 number))
   (atan n1 n2))
 
-#|
-(defparameter *maxima-lisp-table-string*
-  (loop for (m l) in *maxima-lisp-table*
-     collect (list (format nil "~a" m) (format nil "~a" l)))
-  "*maxima-lisp-table* converted to list of strings")
-|#
-
 (defparameter *maxima-lisp-table-intern*
   (loop for (m l) in *maxima-lisp-table*
      collect (list (intern (format nil "~a" m) :maxima) l))
-  "*maxima-lisp-table* interned in :MAXIMA")
-
-#|
-(defparameter *maxima-lisp-table-string-embedded*
-  (loop for (m l) in *maxima-lisp-table-embedded*
-     collect (list (format nil "MAXIMA::~a" m) (format nil "~a" l)))
-  "*maxima-lisp-table* converted to list of strings")
-|#
-
-#|
-(defparameter *regex-symbols*
-  (list "(" ")"
-	"[" "]"
-	"^"
-	"$"
-	"."
-	"|"
-	"?"
-	"*"
-	"+")
-  "regular expression special characters")
-|#
+  "*maxima-lisp-table* interned in :MAXIMA so symbols are recognized")
 
 (defun array-to-matrix (a)
   "Convert a lisp 2D array to Maxima matrix form"
@@ -113,24 +70,6 @@
 	  finally (setq string (concatenate 'string string ")")))
      finally (setq string (concatenate 'string string ")"))
        (return string)))
-
-#|
-(defun run-maxima-command (string)
-  "evaluate maxima expression string"
-  (command-output 
-   "~a ~a ~a;\"" 
-   *maxima-binary* 
-   (apply #'concatenate 'string *maxima-batch-options* *maxima-init-expressions*)
-   string))
-
-(defun run-maxima-lisp (string)
-  "execute a string using maxima's internal lisp, returning last expression as a string"
-  (scan-to-strings "\\n[\\s\\S]*"
-		   (nth (1+ (length *maxima-init-expressions*))
-			(split "\\(%i[\\d]+\\)"
-			       (run-maxima-command 
-				(format nil ":lisp ~a" string))))))
-|#
 
 (defun matrix-to-array (mm)
   "Convert maxima matrix as list expression to lisp 2D array"
@@ -158,53 +97,12 @@
   "Regular expression match of (function args). Doesn't match ((maximafun) args). Returns list of matches."
   (remove-duplicates (all-matches-as-strings "\\([^\\(^\\)^\\s]+ [^\\(^\\)]+\\)" string) :test #'equal))
 
-#|
-(defun match-re-lisp-funs (string)
-  "Match renamed lisp functions"
-  (all-matches-as-strings "#:\\|\\([^\\(^\\)^\\s]+ [^\\(^\\)]+\\)\\|" string))
-
-(defun regexify-parens (string)
-  "Add \\ to parentheses in a string"
-  (regex-replace-all "\\(" (regex-replace-all "\\)" string "\\\\)")  "\\\\("))
-
-(defun regexify-special (string char)
-  "Add \\ to regular expression special character in a string to make it a literal"
-  (regex-replace-all (concatenate 'string "\\" char) string (concatenate 'string "\\\\" char)))
-
-(defun regexify-specials (string)
-  "Add \\ to all regular expression special characters in a string to make them literal"
-  (loop for char in *regex-symbols*
-     for new-string = (regexify-special string char)
-     then (regexify-special new-string char)
-     finally (return new-string)))
-|#
-
-#|
-(defun rename-lisp-funs (maxima-string lisp-funs ren-funs)
-  "Rename lisp functions (non-math) in a Maxima expression to something that evaluates as a symbol in Maxima's lisp mode"
-  (loop with out-string = maxima-string
-     for lisp-fun in lisp-funs
-     for ren-fun in ren-funs
-     do (setq out-string (regex-replace-all (regexify-parens lisp-fun) out-string ren-fun))
-     finally (return out-string)))
-|#
-
 (defun rename-lisp-funs-embedded (mexpr lfuns ren-funs)
   (loop with out-expr = (copy-tree mexpr)
      for lfun in lfuns
      for ren-fun in ren-funs
      do (setq out-expr (nsubst (read-from-string ren-fun) (read-from-string lfun) out-expr :test #'equal))
      finally (return out-expr)))
-
-#|
-(defun re-rename-lisp-funs (maxima-string lisp-funs ren-funs)
-  "Replace renamed lisp functions with original expressions"
-  (loop with out-string = maxima-string
-     for ren-fun in ren-funs
-     for lisp-fun in lisp-funs
-     do (setq out-string (regex-replace-all ren-fun out-string lisp-fun))
-     finally (return out-string)))
-|#
 
 (defun re-rename-lisp-funs-embedded (mexpr lfuns rfuns)
   (loop with outexpr = (copy-tree mexpr)
@@ -220,71 +118,11 @@
      then (nsubst (list m) l mexpr)
      finally (return mexpr)))
 
-#|
-(defun lisp-to-maxima-string (lisp-string)
-  "convert lisp string to maxima string"
-  (let ((maxima-string (remove #\Newline lisp-string)))
-    (loop for (maxima lisp) in *maxima-lisp-table-string*
-       do (setq maxima-string
-		(regex-replace-all ; only replace with ( prefix & space suffix
-		 (regexify-specials (concatenate 'string "(" lisp " "))
-		 maxima-string 
-		 (concatenate 'string "((" maxima ") "))))
-    maxima-string))
-
-(defun lisp-to-maxima-string-embedded (lisp-string)
-  "convert lisp string to maxima string"
-  (let ((maxima-string (remove #\Newline lisp-string)))
-    (loop for (maxima lisp) in *maxima-lisp-table-string-embedded*
-       do (setq maxima-string
-		(regex-replace-all ; only replace with ( prefix & space suffix
-		 (regexify-specials (concatenate 'string "(" lisp " "))
-		 maxima-string 
-		 (concatenate 'string "((" maxima ") "))))
-    maxima-string))
-
-(defun maxima-to-lisp-string (maxima-string)
-  "convert simplified maxima output string to lisp string"
-  (let* ((*read-default-float-format* 'double-float)
-	 (lisp-string (format nil "~a" (read-from-string maxima-string)))) ; fix Maxima output
-    (loop for (maxima lisp) in *maxima-lisp-table-string*
-       do (setq lisp-string
-		(regex-replace-all (format nil "\\(~a SIMP\\)" maxima) lisp-string lisp)))
-    lisp-string))
-|#
-
 (defun maxima-to-lisp (mexpr)
   (let ((lexpr (copy-tree mexpr)))
     (loop for (maxima lisp) in *maxima-lisp-table-intern*
        do (setq lexpr (nsubst lisp (list maxima 'maxima::simp) lexpr :test #'equal)))
     lexpr))
-
-#|
-(defun simplify-lisp-string (lisp-string)
-  "Convert a lisp expression as a string to maxima, and run in maxima to simplify"
-  (let* ((maxima-string (lisp-to-maxima-string lisp-string))
-	 (lisp-funs (match-lisp-funs maxima-string))
-	 (ren-funs (loop for lisp-fun in lisp-funs collect (format nil "~a" (gensym)))))
-    (maxima-to-lisp-string
-     (re-rename-lisp-funs
-      (run-maxima-lisp
-       (format nil "(simplify '~a)"
-	       (rename-lisp-funs maxima-string lisp-funs ren-funs)))
-      lisp-funs
-      ren-funs))))
-
-(defun simplify-lisp-string-embedded (lisp-string)
-  (let* ((maxima-string (lisp-to-maxima-string-embedded lisp-string))
-	 (lisp-funs (match-lisp-funs maxima-string))
-	 (ren-funs (loop for lisp-fun in lisp-funs collect (format nil "~a" (gensym)))))
-    (maxima-to-lisp-string
-     (re-rename-lisp-funs
-      (format nil "~a" (maxima::simplify 
-			(read-from-string 
-			 (rename-lisp-funs maxima-string lisp-funs ren-funs))))
-      lisp-funs
-      ren-funs))))
-|#
 
 (defun simplify-lisp-expr (lexpr)
   "Simplify an algebraic lisp expression"
@@ -298,42 +136,6 @@
        (rename-lisp-funs-embedded mexpr lfuns rfuns))
       lfuns
       rfuns))))
-
-#|
-(defun simplify-lisp-strings (&rest lisp-strings)
-  "Simplify multiple lisp string expressions"
-  (let* ((maxima-string-list (mapcar #'lisp-to-maxima-string lisp-strings))
-	 (lisp-funs-list (mapcar #'match-lisp-funs maxima-string-list))
-	 (ren-funs-list (loop for lisp-funs in lisp-funs-list
-			   collect (loop for lisp-fun in lisp-funs
-				      collect (format nil "~a" (gensym)))))
-	 (renamed-funs-strings (mapcar #'rename-lisp-funs maxima-string-list lisp-funs-list ren-funs-list))
-	 (maxima-output (run-maxima-lisp (format nil "(mapcar #'simplify '(~{~a ~}))" renamed-funs-strings)))
-	 (maxima-output-list (mapcar #'(lambda (l) (format nil "~a" l)) (read-from-string maxima-output))))
-    (mapcar #'(lambda (output lisp-funs ren-funs)
-		(maxima-to-lisp-string
-		 (re-rename-lisp-funs output lisp-funs ren-funs)))
-	    maxima-output-list
-	    lisp-funs-list
-	    ren-funs-list)))
-
-(defun simp (lisp-expr)
-  "Simplify a mathematical lisp expression"
-  (if *delay* 
-      lisp-expr
-      (let ((*read-default-float-format* 'double-float))
-	  (read-from-string (simplify-lisp-string (format nil "~a" lisp-expr))))))
-
-(defun simp-exprs (&rest exprs)
-  "Simplify a list of mathematical Lisp expressions"
-  (if *delay*
-      exprs
-      (let ((*read-default-float-format* 'double-float))
-	(mapcar #'read-from-string 
-		(apply #'simplify-lisp-strings 
-		       (mapcar #'(lambda (expr) (format nil "~a" expr)) 
-			       exprs))))))
-|#
 
 (defmacro delay (&body body)
   "Delay simplification of a block of code"
